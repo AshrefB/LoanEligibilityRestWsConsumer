4c3c6f65d0bc51126a0cf244326d16f9db5c4e26
Achref Bouadila <a_bouadila@live.fr>
2020-02-15 22:57:52 +0100

    create request and response classes

---
+++ src/main/java/com/ashref/loan/rest/ws/payload/requests/CustomerRequest.java
@@ -0,0 +1,12 @@
+package com.ashref.loan.rest.ws.payload.requests;
+
+import lombok.Data;
+
+@Data
+public class CustomerRequest {
+	private String customerName;
+	private int age;
+	private long yearlyIncome;
+	private int cibilScore;
+	private String employmentMode;
+}
---
+++ src/main/java/com/ashref/loan/rest/ws/payload/responses/WsResponse.java
@@ -0,0 +1,13 @@
+package com.ashref.loan.rest.ws.payload.responses;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import lombok.Data;
+
+@Data
+public class WsResponse {
+	private boolean isEligible;
+	private long approvedAmount;
+	private List<String> criteriaMismatch = new ArrayList<>();
+}
