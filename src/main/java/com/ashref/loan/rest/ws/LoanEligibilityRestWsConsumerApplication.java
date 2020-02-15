894b2a197442f5d4c08693a9c28385ecac883db3
Achref Bouadila <a_bouadila@live.fr>
2020-02-15 22:53:41 +0100

    first commit

---
+++ src/main/java/com/ashref/loan/rest/ws/LoanEligibilityRestWsConsumerApplication.java
@@ -0,0 +1,13 @@
+package com.ashref.loan.rest.ws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoanEligibilityRestWsConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoanEligibilityRestWsConsumerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ashref/loan/rest/ws/LoanEligibilityRestWsConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ashref.loan.rest.ws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoanEligibilityRestWsConsumerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
